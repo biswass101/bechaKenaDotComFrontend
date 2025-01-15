18ef02e662987eae577f1bce45becf86155d4413
Emon hossen <emonhossen1000@gmail.com>
2025-01-16 01:36:33 +0600

    version 1.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const App = () => {
   return (
+    <>
     <h1 className='bg-red-500'>
       bechaKena.com
     </h1>
+
+    <h2 className='bg-blue-400'>This is from blue </h2>
+    </>
   );
 };
 
