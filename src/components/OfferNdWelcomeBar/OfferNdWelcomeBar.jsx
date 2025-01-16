033b17112010c00f66996a887533352f1bf562a8
Emon hossen <emonhossen1000@gmail.com>
2025-01-16 23:00:19 +0600

    font family added

--- src/components/OfferNdWelcomeBar/OfferNdWelcomeBar.jsx
+++ src/components/OfferNdWelcomeBar/OfferNdWelcomeBar.jsx
@@ -7,8 +7,8 @@ import { MdWavingHand } from 'react-icons/md'
 
 const OfferNdWelcomeBar = () => {
   return (
-    <div className="offer-welcome-container bg-green-500">
-        <div className="contnt flex justify-between text-2xl font-semibold text-white py-3 px-10">
+    <div className="offer-welcome-container  bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...  ">
+        <div className="contnt flex justify-between text-xl font-semibold py-3 px-10 text-white">
             <div className="location-info flex justify-center items-center gap-5 ">
                 <div className="location flex items-center justify-center gap-2 ">
                     <p className='pb-1'>Location</p>
@@ -21,11 +21,11 @@ const OfferNdWelcomeBar = () => {
                 </div>
             </div>
             <div className="running-offer-slider flex gap-5 items-center justify-center">
-                <FaRegArrowAltCircleLeft/>
+                <FaRegArrowAltCircleLeft className='text-3xl'/>
                 <div className="offer-heading">
                     <p className='pb-1'>Free Shipping On All Orders Over $150</p>
                 </div>
-                <FaRegArrowAltCircleRight/>
+                <FaRegArrowAltCircleRight className='text-3xl'/>
             </div>
             <div className="welcome-message flex gap-2 items-center justify-center">
                 <p className='pb-1'>Welcome To BechaKena</p>
