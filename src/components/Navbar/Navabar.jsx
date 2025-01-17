3c4a749e0f979e2c70ab77b517c6cde4e15c3bcd
biswass101 <biswassnaeemcse@gmail.com>
2025-01-17 16:33:47 +0600

    category and search design added

--- src/components/Navbar/Navabar.jsx
+++ src/components/Navbar/Navabar.jsx
@@ -11,7 +11,7 @@ const Navabar = () => {
   return (
     <>
       <div className="container ">
-        <div className="cntnt flex justify-around border">
+        <div className="cntnt flex justify-around ">
           {/* part 1 */}
           <div className="user-profile-cart-love flex items-center  gap-3  ">
             <div className="user-profile transition-colors duration-200 ease-in-out hover:bg-black border rounded-full border-gray-500  ">
@@ -41,7 +41,7 @@ const Navabar = () => {
           </div>
           {/* done  */}
 
-          <div className="search-bar bg-slate-400 flex justify-between items-center rounded-full w-[50%]">
+          <div className="search-bar bg-white border px-2 border-gray-300 flex justify-between items-center rounded-full w-[50%]">
             <div className="seatc-icon-container bg-blue-600 transition-colors duration-200 ease-in-out hover:bg-black hover:text-white rounded-full p-4">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -53,23 +53,28 @@ const Navabar = () => {
                 <path d="M80-140v-320h320v320H80Zm80-80h160v-160H160v160Zm60-340 220-360 220 360H220Zm142-80h156l-78-126-78 126ZM863-42 757-148q-21 14-45.5 21t-51.5 7q-75 0-127.5-52.5T480-300q0-75 52.5-127.5T660-480q75 0 127.5 52.5T840-300q0 26-7 50.5T813-204L919-98l-56 56ZM660-200q42 0 71-29t29-71q0-42-29-71t-71-29q-42 0-71 29t-29 71q0 42 29 71t71 29ZM320-380Zm120-260Z" />
               </svg>
             </div>
-            <div className="seaarch-ipt-category flex justify-between w-[70%]">
-              <form className="flex items-center border gap-3 border-gray-300 rounded-lg p-2 w-[60%]">
+            <div className="seaarch-ipt-category flex gap-2 justify-between w-[80%]">
+              <form className="flex items-center border border-none gap-3  rounded-lg w-[80%]">
                 <input
                   type="text"
-                  placeholder="Search for products..."
-                  className="outline-none bg-transparent text-gray-700 w-full"
+                  placeholder="Search for keywords, Products..."
+                  className="outline-none bg-transparent text-gray-700 w-full border-none"
                 />
               </form>
-              <div className="category w-[40%]">
-                <div className="dropdown w-full bg-black">
-                  <div tabIndex={0} role="button" className="btn m-1">
-                    <FaCaretDown /> Click
+              <div className="category w-[40%] bg-transparent rounded-r-full">
+                <div className="dropdown w-full">
+                  <div tabIndex={0} role="button" className="btn w-full rounded-full bg-black text-white hover:bg-black hover:text-white">
+                    <FaCaretDown /> Choose Category
                   </div>
                   <ul
                     tabIndex={0}
                     className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
                   >
+                    <input 
+                      type="text"
+                      placeholder="Search Category"
+                      className="outline-none rounded-full p-2 text-right placeholder:p-2 bg-transparent border border-gray-700 text-gray-700 w-full"
+                    />
                     <li>
                       <a>Item 1</a>
                     </li>
